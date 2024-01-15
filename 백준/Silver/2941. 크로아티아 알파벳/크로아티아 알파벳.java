39f6aaaeca1b93931a43af92fbd15ed6c393f597
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-01-15 11:57:51 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        String newStr = filterCro(input);
+
+        bw.write(String.valueOf(newStr.length()));
+        bw.flush();
+    }
+
+    static String filterCro(String s) {
+        String input = s;
+        List<String> cros = new ArrayList<>(List.of("c=","c-","dz=","d-","lj","nj","s=","z="));
+
+        for (String cro : cros) {
+            if(input.contains(cro)){
+               input = input.replace(cro,"!");
+            }
+        }
+        return input;
+    }
+}
