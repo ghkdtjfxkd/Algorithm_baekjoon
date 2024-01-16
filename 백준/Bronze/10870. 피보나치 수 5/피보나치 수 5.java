040aed751cf937bf9d6147aece7b671a8f3c4b87
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-01-16 23:17:54 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+
+
+        fibonacci(count, 0 , 1);
+
+
+    }
+
+    static int fibonacci (int count, int now, int next) {
+        if (count == 0){
+            System.out.println(now);
+            return 0;
+        }
+        else {
+            return fibonacci(count-1, next, now + next);
+        }
+    }
+}
