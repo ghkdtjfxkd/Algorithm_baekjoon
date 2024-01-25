dc2144a1d7fe80abfe243d657172684cab1c50ef
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-01-25 11:04:21 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 376 ms, Memory: 18404 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputSize = br.readLine().split(" ");
+
+        int row = Integer.parseInt(inputSize[0]);
+        int col = Integer.parseInt(inputSize[1]);
+
+        int[][] first = new int[row][col];
+        int[][] second = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            String[] rowInput = br.readLine().split(" ");
+            for (int j = 0; j < col; j++) {
+                first[i][j] = Integer.parseInt(rowInput[j]);
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            String[] rowInput = br.readLine().split(" ");
+            for (int j = 0; j < col; j++) {
+                second[i][j] = Integer.parseInt(rowInput[j]);
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(first[i][j] + second[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
+}
