fc781b2be05bb18ada3a8251836d6663ebb0dfde
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-01-30 21:45:19 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        bw.write("         ,r'\"7\n");
+        bw.write("r`-_   ,'  ,/\n");
+        bw.write(" \\. \". L_r'\n");
+        bw.write("   `~\\/\n");
+        bw.write("      |\n");
+        bw.write("      |");
+        bw.flush();
+    }
+}
