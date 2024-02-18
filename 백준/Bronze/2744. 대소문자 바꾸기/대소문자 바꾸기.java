4427281de7a52d222149e56b90b5c8374e08c602
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-02-18 19:44:06 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 136 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split("");
+        StringBuilder sb = new StringBuilder();
+        for (String s : input) {
+            if(s.matches("[a-z]")){
+                sb.append(s.toUpperCase());
+                continue;
+            }
+            if(s.matches("[A-Z]")){
+                sb.append(s.toLowerCase());
+            }
+        }
+        System.out.println(sb);
+    }
+}
