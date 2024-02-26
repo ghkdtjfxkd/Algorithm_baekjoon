958621e783dbfc17fa2ea1f522fed1c5af5ae9d5
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-02-26 11:50:04 +0900

    [Silver V] Title: 막대기, Time: 136 ms, Memory: 14680 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int x = Integer.parseInt(br.readLine());
+
+        List<Double> stickList = new ArrayList<>();
+        stickList.add(64.0);
+        System.out.println(makeStick(x,stickList));
+    }
+
+    static int makeStick (int target , List<Double> sticks){
+        if(sticks.stream().mapToDouble(n->n).sum() == target){
+            return sticks.size();
+        }else {
+            double min = sticks.stream().mapToDouble(n -> n).min().getAsDouble();
+            sticks.remove(min);
+            sticks.add(min/2);
+            
+            if(sticks.stream().mapToDouble(n->n).sum() < target){
+                sticks.add(min/2);
+            }
+            return makeStick(target,sticks);
+        }
+    }
+}
