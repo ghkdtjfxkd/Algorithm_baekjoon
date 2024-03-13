5560fc8fc4be4acec41e8d54460f1d94a622353e
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-03-14 08:38:51 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int pal = 1;
+        StringBuilder sb = new StringBuilder();
+
+        String[] input = br.readLine().split("");
+
+        for (int i = 0; i < input.length / 2 ; i++) {
+            if(!input[i].equals(input[input.length - i -1])){
+                pal = 0;
+                break;
+            }
+        }
+        System.out.println(pal);
+    }
+}
