3a0732fd107264acfcfcc670d5bc70c776f95636
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-03-18 13:54:37 +0900

    [Bronze II] Title: 커트라인, Time: 132 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[] scores = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(scores);
+        System.out.println(scores[n-k]);
+    }
+}
