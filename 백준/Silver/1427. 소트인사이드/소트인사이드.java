243fde01ecb9f243dfdce9a51d94f46d1cb67919
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-03-18 13:58:41 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String[] input = br.readLine().split("");
+
+        Arrays.sort(input, Collections.reverseOrder());
+        Arrays.stream(input).forEach(sb::append);
+        System.out.println(sb);
+    }
+}
