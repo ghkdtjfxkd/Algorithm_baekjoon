e8b70392026410cf2a1151254e8b07037fdcb3c6
youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2024-03-21 12:20:59 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static Integer[] dp;
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        dp = new Integer[n+1];
+        System.out.println(recur(n));
+    }
+
+    static int recur(int n) {
+
+        if(dp[n] == null){
+            if(n == 0){
+                dp[n] = 1;
+            } else if(n == 1){
+                dp[n] = 1;
+            } else {
+                dp[n] = recur(n-1) + recur(n-2);
+            }
+        }
+        return dp[n] % 10007;
+    }
+
+}
