48240744558eb4614eb813f2c044970b4667f71b
youngtak son <144784450+ghkdtjfxkd@users.noreply.github.com>
2026-02-09 22:49:27 +0900

    [Silver III] Title: N과 M (4), Time: 188 ms, Memory: 22756 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        dfs(0, 1, m, "");
+        bw.flush();
+    }
+
+    static void dfs(int pre, int idx, int blank, String seq) throws IOException {
+        if(pre > idx) return;
+        if(blank == 0) {
+            bw.write(seq.substring(1));
+            bw.write("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            dfs(idx, i, blank - 1, seq + " " + i);
+        }
+    }
+}
