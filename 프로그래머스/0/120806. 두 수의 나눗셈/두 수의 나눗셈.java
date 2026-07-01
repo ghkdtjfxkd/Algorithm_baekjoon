74efe92f21646a4d8fa5deae9528ead35da588ee
Youngtak Son <144784450+ghkdtjfxkd@users.noreply.github.com>
2026-07-01 15:47:03 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,5 +1,7 @@
+import java.util.*;
 class Solution {
     public int solution(int num1, int num2) {
-        return (int) ((double) num1 / (double) num2  * 1000);
+        double val = (double) num1 / (double) num2 * 1000;
+        return (int) val;
     }
 }
